accf4055759f0ba9edb1d4de0a65b605cc7ce6c1
Andreas Pfeil <andreas.pfeil@student.kit.edu>
2018-10-12 14:52:13 +0200

    Cargo fmt and more flexible versions in cargo.toml.

--- examples/print.rs
+++ examples/print.rs
@@ -7,14 +7,14 @@ fn main() -> Result<(), Box<std::error::Error>> {
     let args = env::args().collect::<Vec<String>>();
     let path = args.get(1);
     let path = path.expect("You need to give a path to a camt csv file as first argument.");
-    
+
     match from_file(path) {
         Ok(transactions) => {
             for t in &transactions {
                 println!("{:?}\n", t);
             }
             Ok(())
-        },
+        }
         Err(e) => Err(e),
     }
 }
